08b9320b2225a6c91f2346642b0ef8a58296e7fa
Sid <siddharthmani@sfhs.com>
2016-05-07 17:06:54 -0700

    Added .gitignore, Stars class working.

---
+++ src/Stars.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
+
+/**
+ * Created by Sid on 5/7/16.
+ */
+public class Stars extends JComponent {
+    ArrayList<Point2D.Double> points = new ArrayList<Point2D.Double>();
+    public Stars(int num, int width, int height) {
+        for (int i = 0; i < num; i++) {
+            points.add(new Point2D.Double(Math.random()*width, Math.random()*height));
+        }
+    }
+
+    public void paintComponent(Graphics g) {
+        Graphics g2 = (Graphics2D) g;
+        g2.setColor(Color.WHITE);
+        for(Point2D.Double p : points) {
+            g2.drawOval((int)p.x, (int)p.y, 4, 4);
+        }
+    }
+}
+
